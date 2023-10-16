9a685f5dc4385f3c67a437d3cf4e3e9ce2e53838
Perion Demaman <periondemaman@gmail.com>
2023-10-15 23:09:46 -0300

    inserting the apis from graphql

---
+++ apis/constants/muscle_type.ts
@@ -0,0 +1,28 @@
+export enum LEGS {
+  QUADRICEPS = 'Quadriceps',
+  HAMSTRINGS = 'Hamstrings',
+  GLUTES = 'Glutes',
+  CALVES = 'Calves',
+}
+
+export enum CHEST {
+  UPPER = 'Upper',
+  LOWER = 'Lower',
+}
+
+export enum BACKS {
+  LATISSIMUS = 'Latissimus',
+  TRAPEZIUS = 'Trapezius',
+  RHOMBOIDS = 'Rhomboids',
+}
+
+export enum SHOULDERS {
+  ANTERIOR = 'Anterior',
+  LATERAL = 'Lateral',
+  FRONTAL = 'Frontal',
+}
+
+export enum ARMS {
+  BICEPS = 'Biceps',
+  TRICEPS = 'Triceps',
+}
